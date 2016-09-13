b168c3504c3b88a4f7c3fa55a40dd98443026ce0
Silvan Herrema <silvah489@gmail.com>
2016-09-13 11:31:53 +0200

    Vector Extensions Vector2.Rotate

---
+++ VectorExtensions.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class VectorExtensions
+{
+    public static Vector2 Rotate(this Vector2 vector, float degrees)
+    {
+        float radians = degrees * Mathf.Deg2Rad;
+        float sin = Mathf.Sin(radians);
+        float cos = Mathf.Cos(radians);
+
+        float x = cos * vector.x - sin * vector.y;
+        float y = sin * vector.x + cos * vector.y;
+        return new Vector2(x, y);
+    }
+}
